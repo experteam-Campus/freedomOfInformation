b5864bf1dd3f848080f6c05e69223b1a42b893a5
experteam-Campus <89127414+experteam-Campus@users.noreply.github.com>
2022-12-26 13:25:16 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# freedomOfInformation
+# Project-Management
+ 
---
+++ courseScript.js
@@ -0,0 +1,52 @@
+/*$(document).ready(function() {
+
+    window.onscroll = function() { scrollIndicator() };
+
+    function scrollIndicator() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("scrollIndicatorBar").style.width = scrolled + "%";
+    }
+
+
+});
+
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }*/
+
+    window.onscroll = function() { scrollIndicator() };
+
+    function scrollIndicator() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("scrollIndicatorBar").style.width = scrolled + "%";
+    }
+
+    console.log("helllo");
+
+
+
+
+           
+
+
+
+
+    
